70507391e9b7f40ee42fcd3b9dd9e2967ea3f92d
sechelc <cristian.sechel@yahoo.ro>
2016-06-01 20:37:01 +0300

    'oare?'

---
+++ src/main/java/com/rogames/engine/controller/WebPingcontroller.java
@@ -0,0 +1,16 @@
+package com.rogames.engine.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class WebpingController {
+
+    @RequestMapping("/webping")
+    @ResponseBody
+    public String webping(){
+        return "OK";
+    }
+
+}
